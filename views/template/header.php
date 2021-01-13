4ef09d13d3395cee1f0fcb095fa8679715ddbca7
Zawatsuka <57181738+Zawatsuka@users.noreply.github.com>
2021-01-13 14:22:17 +0100

    maj

--- views/template/header.php
+++ views/template/header.php
@@ -6,5 +6,12 @@
     <title>Document</title>
 </head>
 <body>
-
+<header>
+        <div>
+            <a href="index.php?id=l">Page 0</a>
+            <a href="index.php?id=2">Page 1</a>
+            <a href="index.php?id=3">Page 2</a>
+            <a href="index.php?id=4">Page 3</a>
+        </div>
+    </header>
 
