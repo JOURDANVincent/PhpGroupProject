22c017f98a42723e8b3f6cf10a383072f6667852
Lendesz <lendesz@gmail.com>
2021-01-13 10:52:51 +0100

    depot initial

---
+++ functions/XML_TO_JSON-CONVERTER.php
@@ -0,0 +1,33 @@
+<?php
+
+$url = 'assets/json/recipe/test.xml';
+
+$json = XML_TO_JSON($url);
+
+//var_dump($json);
+
+
+function XML_TO_JSON ($url) {
+
+    $fileContents= file_get_contents($url);
+
+    $fileContents = str_replace(array("\n", "\r", "\t"), '', $fileContents);
+
+    $fileContents = trim(str_replace('"', "'", $fileContents));
+
+    $simpleXml = simplexml_load_string($fileContents);
+
+    //var_dump($simpleXml);
+
+    $json = json_encode($simpleXml);
+
+    return $json;
+
+}
+
+?>
+
+
+
+
+
