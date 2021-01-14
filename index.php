9937535fbecb66e5146e1f8e80db5e2cb24de50a
Lendesz <lendesz@gmail.com>
2021-01-14 11:36:16 +0100

    maj xml page 4

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     // affichage de la page sélectionnée
     echo '<main>
             <div class="container">
-                <div class="row">
+                <div class="row justify-content-center align-items-center">
                     <div class="col-12 py-5" id="page'.$id.'">
                         '.$contentPage.'
                     </div>
